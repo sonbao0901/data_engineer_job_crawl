a21a998375d889c2408af34597186309cdfa516d
sonbao0901 <phanb688@gmail.com>
2025-03-30 12:36:16 +0700

    test call db from Render

--- test/call_render_db.py
+++ test/call_render_db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, text
-
+db_conn = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 conn = create_engine(db_conn).connect()
 
 r = conn.execute(text("""
