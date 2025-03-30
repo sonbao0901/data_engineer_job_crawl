3adfa18da0edff979dc545c4147336fee638523c
sonbao0901 <phanb688@gmail.com>
2025-03-30 12:37:07 +0700

    change connection airflow db from local to render db

--- entrypoint/entrypoint_airflow.sh
+++ entrypoint/entrypoint_airflow.sh
@@ -3,7 +3,7 @@ set -e
 
 # Wait for the database to be ready
 echo "Waiting for database..."
-until pg_isready -h "$DB_HOST" -p 5432 -U "$DB_USER"; do
+until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
   sleep 5
 done
 echo "Database is ready."
