f3a19bbce79c2afd88a23bb55d1dd476cf87dd55
sonbao0901 <phanb688@gmail.com>
2025-04-13 14:44:28 +0700

    update test

--- test/call_render_db.py
+++ test/call_render_db.py
@@ -1,5 +1,10 @@
 from sqlalchemy import create_engine, text
-db_conn = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_conn = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@localhost:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 conn = create_engine(db_conn).connect()
 
 r = conn.execute(text("""
